b9c0e6aac018b7f9acbe9a044d54f561276bc2bd
orange231 <mail@ewenharrison.combob.finlayson32@gmail.comgit config --global user.name ewenharrisonorange231>
2017-10-15 13:46:56 +0100

    Initial Commit

---
+++ test.R
@@ -0,0 +1,12 @@
+library(quantmod)
+getSymbols("AAPL")
+price_AAPL <- AAPL[,6]
+plot(price_AAPL, main = "The price of AAPL")
+returns_AAPL <- diff(log(price_AAPL))
+plot(returns_AAPL, main = "AAPL % returns", col = "navyblue")
+hist(returns_AAPL, breaks = 100, col="brown")
+acf(returns_AAPL[-1], main = "Autocorrelation plot of returns")
+mR  <- mean(returns_AAPL[-1])
+sdR <- sd(returns_AAPL[-1])
+mR
+sdR
